8e78a3cb31849bf5446b0119831f5b06a3e7ec42
Aaron Rafter <raftera@oregonstate.edu>
2021-05-02 04:07:19 -0700

    full test_listavg testing implemented

--- test_listavg.py
+++ test_listavg.py
@@ -3,7 +3,17 @@ import listavg
 
 class TestCase(unittest.TestCase):
      def test_listavg(self):
-         result = listavg.average(1)
+         list = (1, 2, 3)
+         result = listavg.average(list)
+         self.assertEqual(result, 2)
+     def test_listavg2(self):
+          list = (0, 0, 0)
+          result = listavg.average(list)
+          self.assertEqual(result, 0)
+     def test_listavg3(self):
+          list = (this, should, error)
+          result = listavg.average(list)
+          self.assertEqual(result, 0)
            
 
 
