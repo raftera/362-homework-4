9ac0a94390eb621319c199cbbc0acccdec2d56de
Aaron Rafter <raftera@oregonstate.edu>
2021-05-02 04:17:11 -0700

    implemented full cubevol testing

--- cubevol.py
+++ cubevol.py
@@ -4,19 +4,19 @@ def volume(side1, side2, side3):
 
 
      if side1 < 1:
-            print("No side can be less than 1.")
+          #  print("No side can be less than 1.")
             return 0
      if side2 < 1:
-            print("No side can be less than 1.")
+        #    print("No side can be less than 1.")
             return 0
      if side3 < 1:
-            print("No side can be less than 1.")
+         #   print("No side can be less than 1.")
             return 0
 
      return side1*side2*side3
 
     else:
-         print("All inputs must be ints.")
+     #    print("All inputs must be ints.")
          return 0
 
     
--- test_cubecol.py
+++ test_cubecol.py
@@ -6,6 +6,12 @@ class TestCase(unittest.TestCase):
     def test_cubevol(self):
         result = cubevol.volume(3, 3, 3)
         self.assertEqual(result, 27)
+    def test_cubevol2(self):
+        result = cubevol.volume(3, 3, 0)
+        self.assertEqual(result, 1)
+    def test_cubevol3(self):
+        result = cubevol.volume("this", "should", "error")
+        self.assertEqual(result, 1)
 
 
 
